839b7544ab98de8e27508e660aff1cab9b98a97b
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-19 19:44:19 +0200

    Prepare js logic

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("js-init");
+"use strict";
+
+let brandInput, modelInput, yearInput, errorInfo, addBtn, carList;
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  brandInput = document.querySelector("#brand-input");
+  modelInput = document.querySelector("#model-input");
+  yearInput = document.querySelector("#year-input");
+  errorInfo = document.querySelector(".error-info");
+  addBtn = document.querySelector(".btn-add");
+  carList = document.querySelector(".carlist tbody");
+};
+
+const prepareDOMEvents = () => {};
+
+const addNewCar = () => {};
+
+const createToolsArea = () => {};
+
+const editCar = () => {};
+
+const deleteCar = () => {};
+
+document.addEventListener("DOMContentLoaded", main);
